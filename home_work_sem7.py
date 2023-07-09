d21e2b96aed6a1a6b0f99a9f6e4340e881c8c566
PC-AlekseyB <bukalov@bk.ru>
2023-07-09 16:25:36 +0300

    Выполнил домашнее задание к семинару 7 по курсу знакомство с языком Python

--- home_work_sem7.py
+++ home_work_sem7.py
@@ -12,21 +12,21 @@
 
 # Решение
 
-def rhythm(str):
-    str = str.split()
-    list_1 = []
-    for word in str:
-        sum_w = 0
-        for i in word:
-            if i in 'ауоыиэяюёе':
-                sum_w += 1
-        list_1.append(sum_w)
-        return len(list_1) == list_1.count(list_1[0])
-str_1 = 'пара-ра-рам рам-пам-папам па-ра-па-да'
-if rhythm(str_1):
-    print('Парам пам-пам')
-else:
-    print('Пам парам')
+# def rhythm(str):
+#     str = str.split()
+#     list_1 = []
+#     for word in str:
+#         sum_w = 0
+#         for i in word:
+#             if i in 'ауоыиэяюёе':
+#                 sum_w += 1
+#         list_1.append(sum_w)
+#         return len(list_1) == list_1.count(list_1[0])
+# str_1 = 'пара-ра-рам рам-пам-папам па-ра-па-да'
+# if rhythm(str_1):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
 
 
 # Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
